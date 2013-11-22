8badcc36ded5ccc6576da0a2f11e121184695e4a
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-22 12:46:21 -0500

    Add test to check for instance variable of pokemon.rb without attribute accessor

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -3,9 +3,15 @@ require_relative '../lib/pokemon'
 describe Pokemon, "#name" do
 	test_pokemon = Pokemon.new("Keithachu")
 
-	it "should respond to name (getter)" do
-		expect(test_pokemon.name).to eq("Keithachu")
-	end
+	# it "should respond to name (getter)" do
+	# 	expect(test_pokemon.name).to eq("Keithachu")
+	# end
+
+	it "should have instance variable name" do
+		#http://ruby-doc.org/core-1.9.3/Object.html#method-i-instance_variable_set
+		expect(test_pokemon.instance_variable_get(:@name)).to eq("Keithachu") 
+		end
+
 end
 
 describe Pokemon, "#say_name" do
