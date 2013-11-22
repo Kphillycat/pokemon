8d8f44e3df33a4384f49cf759a99c440f2a16494
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-22 12:52:39 -0500

    Remove unneccessary comments from pokemon.rb and pokemon_spec.rb

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -3,10 +3,6 @@ require_relative '../lib/pokemon'
 describe Pokemon, "#name" do
 	test_pokemon = Pokemon.new("Keithachu")
 
-	# it "should respond to name (getter)" do
-	# 	expect(test_pokemon.name).to eq("Keithachu")
-	# end
-
 	it "should have instance variable name" do
 		#http://ruby-doc.org/core-1.9.3/Object.html#method-i-instance_variable_set
 		expect(test_pokemon.instance_variable_get(:@name)).to eq("Keithachu") 
